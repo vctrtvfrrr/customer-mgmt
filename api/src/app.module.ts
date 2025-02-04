e4f4a7744898ccb3727f8e4cdba2406f5b6a8463
Victor Otávio Ferreira <victor@ferreira.id>
2025-02-04 14:17:54 -0300

    feat(db): add TypeORM config for PostgreSQL

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
